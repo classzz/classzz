e9a0cf63795f5ba80fae1d08c685addcf12ff626
jasonberger11 <icefish0012@gmail.com>
2019-10-01 20:44:01 -0700

    add test unit for KeepedAmount and EntangleTxInfo

--- cross/czzTx_test.go
+++ cross/czzTx_test.go
@@ -4,7 +4,7 @@ import (
 	// "bytes"
 	// "encoding/binary"
 	// "errors"
-	// "math/big"
+	"math/big"
 	"fmt"
 
 	// "github.com/classzz/classzz/chaincfg"
@@ -31,4 +31,43 @@ func TestCalcModel(t *testing.T){
 		doge = doge * int64(i+1)
 		itc = itc * int64(i+1)
 	}
+}
+
+func TestStruct(t *testing.T) {
+	d1 := EntangleTxInfo{
+		ExTxType:	ExpandedTxEntangle_Doge,
+		Index:		10,
+		Height:		200,
+		Amount:		big.NewInt(333311),
+		ExtTxHash:	nil,
+	}
+	sByte := d1.Serialize()
+	fmt.Println("d1.Serialize():",sByte)
+	d2 := EntangleTxInfo{}
+	d2.Parse(sByte)
+	fmt.Println("ExTxType:",d2.ExTxType," Index:",d2.Index," Height:",d2.Height,
+	"Amount:",d2.Amount,"ExtTxHash:",d2.ExtTxHash)
+
+	Sum := byte(10)
+	items := KeepedAmount{
+		Count:	0,
+		Items:	make([]KeepedItem, 0),
+	}
+	for i:=0;i<int(Sum);i++ {
+		v := KeepedItem{
+			ExTxType: 	ExpandedTxEntangle_Doge,
+			Amount:		big.NewInt(int64(100*i)),
+		}
+		items.add(v)
+	}
+	fmt.Println("Count:",items.Count,"items:",items.Items)
+	sByte2 := items.Serialize()
+	fmt.Println("sByte2:",sByte2)
+	items2 := KeepedAmount{}
+	items2.Parse(sByte2)
+	fmt.Println("Count:",items2.Count,"items:",items2.Items)
+}
+
+func TestEntangleTx(t *testing.T) {
+
 }
