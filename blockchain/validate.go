91ddd591edf3cfbf72356729a45fbb2aaee1f30c
jasonberger11 <icefish0012@gmail.com>
2019-11-03 17:30:18 +0800

    update verify subsidy

--- blockchain/validate.go
+++ blockchain/validate.go
@@ -972,12 +972,10 @@ func checkMergeTxInCoinbase(tx *czzutil.Tx, txHeight int32, utxoView *UtxoViewpo
 	}
 	return false, nil
 }
-
-func checkSubsidyForPoolAddr(income1, income2 int64) error {
-	return nil
-}
-
 func checkBlockSubsidy(block, preBlock *czzutil.Block, txHeight int32, utxoView *UtxoViewpoint, amountSubsidy int64, chainParams *chaincfg.Params) error {
+	if txHeight <=1 {
+		return nil
+	}
 	originIncome1, originIncome2 := amountSubsidy*19/100, amountSubsidy/100
 	originIncome3 := amountSubsidy - originIncome1 - originIncome2
 	if txHeight == chainParams.EntangleHeight {
