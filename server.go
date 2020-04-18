4fae8efa2f23f1b84f3b80f112ef4ac07b056bc1
matthew-pro <matthew-pro@protonmail.com>
2020-04-18 23:16:06 +0800

    add Peer constraint

--- server.go
+++ server.go
@@ -488,7 +488,7 @@ func (sp *serverPeer) OnVersion(_ *peer.Peer, msg *wire.MsgVersion) *wire.MsgRej
 		return wire.NewMsgReject(msg.Command(), wire.RejectNonstandard, reason)
 	}
 
-	if strings.Contains(msg.UserAgent, "classzz:2.0") || strings.Contains(msg.UserAgent, "classzz:1.0") || strings.Contains(msg.UserAgent, "classzz:0.0") {
+	if strings.Contains(msg.UserAgent, "classzz:2.") || strings.Contains(msg.UserAgent, "classzz:1.0") || strings.Contains(msg.UserAgent, "classzz:0.0") {
 		srvrLog.Debugf("Rejecting peer %s for running < v3.0.0", sp.Peer)
 		reason := fmt.Sprint("Not >= v3.0.0 node")
 		return wire.NewMsgReject(msg.Command(), wire.RejectNonstandard, reason)
