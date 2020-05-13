95b7622fdea6836be83d453e2b372272860bc990
Wilson-ton <Wilson-ton@protonmail.com>
2020-05-13 17:26:28 +0800

    add checkpoint

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -264,10 +264,12 @@ var MainNetParams = Params{
 		{Height: 225000, Hash: newHashFromStr("fc0e1538e7369862e41c8cedbf2b0b73eaf1a3f76cdad7496ec7884e3aad3e9d")},
 		{Height: 260000, Hash: newHashFromStr("c37dfc73a91484ef4279f67d14aeb7537c32203cc7463ea871877ead64af57eb")},
 		{Height: 320757, Hash: newHashFromStr("b5e2b5431de01a93efbcdef77b5a72ba216dcdcf14728e66e22d96387da3089b")},
+		{Height: 350757, Hash: newHashFromStr("19d78e5c64bd4f414ca13765dd7ff7639e3b58749969fd650170ff10db6b8b92")},
 		{Height: 370000, Hash: newHashFromStr("80258d53649075b0118ac709faa639061b1fc5780091487b71ec1dd9b1db2e6b")},
 		{Height: 400100, Hash: newHashFromStr("7c7fd0304aa4409c091a7681e4b094dcf82fe497b6831bf96c7ea31352c757f2")},
 		{Height: 422555, Hash: newHashFromStr("095dd0703fb7ece922bb157cfaa5ccd25469e6a95bf1fc226d734c09722d6e64")},
 		{Height: 436000, Hash: newHashFromStr("57f6f4ead9aee8becc9acab3fad18c6194684069e3ca781c64ce79c22bca83f4")},
+		{Height: 466000, Hash: newHashFromStr("0f97de060983f83533820977754b279d66a9085ed6cb2a628448bec7c9cbd0ad")},
 	},
 
 	// Consensus rule change deployments.
